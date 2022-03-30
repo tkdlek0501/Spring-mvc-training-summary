7529a5cab37c4ea3c086860d0acd5545ba16be1b
hj0501 <tkdlek0501@naver.com>
2022-03-30 15:13:04 +0900

    validation v1 to v2

--- src/main/java/hello/itemservice/web/basic/ProductController.java
+++ src/main/java/hello/itemservice/web/basic/ProductController.java
@@ -1,6 +1,7 @@
 package hello.itemservice.web.basic;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,6 +10,7 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,7 +38,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/basic/items")
+//@RequestMapping("/basic/items")
+@RequestMapping("/advanced/items")
 public class ProductController {
 	
 	private final ProductRepository productRepository; 
@@ -78,7 +81,8 @@ public class ProductController {
 	public String items(Model model) {
 		List<Product> items = productRepository.findAll();
 		model.addAttribute("items", items);
-		return "basic/items";
+		//return "basic/items";
+		return "advanced/items";
 	}
 	
 	// 상품 상세
@@ -86,7 +90,8 @@ public class ProductController {
 	public String item(@PathVariable long itemId, Model model) {
 		Product item = productRepository.findById(itemId);
 		model.addAttribute("item", item);
-		return "basic/item";
+		//return "basic/item";
+		return "advanced/item";
 	}
 	
 	// 상품 등록 폼
@@ -101,7 +106,8 @@ public class ProductController {
 //		model.addAttribute("regions", regions);
 // 위 코드가 등록뿐만 아니라 수정, 상세에서도 나와야한다. -> 스프링에서는 @ModelAttribute의 기능으로 해결할 수 있다. 클래스 상단에 적용.
 		
-		return "basic/addForm";
+		//return "basic/addForm";
+		return "advanced/addForm";
 	}
 	// 요구사항 추가
 	// 판매 여부(체크박스), 등록 지역(체크박스 다중), 상품 종류(라디오버튼), 배송방식(selectbox)
@@ -113,6 +119,37 @@ public class ProductController {
 			Model model,
 			RedirectAttributes redirectAttributes
 			) {
+		
+		// 검증 오류 결과
+		Map<String, String> errors = new HashMap<>();
+		
+		// 검증 로직
+		if(!StringUtils.hasText(item.getProductName())) {
+			errors.put("itemName", "상품 이름은 필수로 입력해주셔야 합니다.");
+		}
+		if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+			errors.put("price", "가격은 1,000 ~ 1,000,000 까지 허용합니다.");
+		}
+		if(item.getQuantity() == null || item.getQuantity() > 9999) {
+			errors.put("quantity", "수량은 최대 9,999 까지 허용합니다.");
+		}	
+		// 특정 필드가 아닌 복합 검증
+		if(item.getPrice() != null && item.getQuantity() != null) {
+			int resultPrice = item.getPrice() * item.getQuantity();
+			if(resultPrice < 10000) {
+				errors.put("globalError", "가격 x 수량의 합은 10,000 원 이상이어야 합니다. 현재 값 = " + resultPrice);
+			}
+		}
+		
+		// 검증에 실패하면 다시 입력 폼으로
+		if (!errors.isEmpty()) {
+			log.info("errors = {} ", errors);
+			model.addAttribute("errors", errors);
+			// return "basic/addForm";
+			return "advanced/addForm";
+		}
+		
+		// 성공시
 		log.info("product.open={}",item.getOpen());
 		// HTML 에서 체크 박스를 선택하지 않고 form 전송하면 open 이라는 필드 자체가 서버로 전송되지 않음; null이 되어 문제...
 		// -> hidden input을 넣어 해결 => th:filed로 간단히 해결
@@ -124,7 +161,8 @@ public class ProductController {
 		
 		model.addAttribute("item", item);
 		//return "redirect:/basic/items/" + item.getId(); // PRG 
-		return "redirect:/basic/items/{itemId}"; // redirectAttributes에 넣은 itemId 값을 사용 가능
+		//return "redirect:/basic/items/{itemId}"; // redirectAttributes에 넣은 itemId 값을 사용 가능
+		return "redirect:/advanced/items/{itemId}";
 		// 나머지 설정한 값들은 쿼리 파라미터 형식으로 들어간다. ?status=true
 	}
 //		@PostMapping("/add")
@@ -160,14 +198,16 @@ public class ProductController {
 	public String editForm(@PathVariable Long itemId, Model model) {
 		Product item = productRepository.findById(itemId);
 		model.addAttribute("item", item);
-		return "basic/editForm";
+		//return "basic/editForm";
+		return "advanced/editForm";
 	}
 	
 	// 상품 수정
 	@PostMapping("{itemId}/edit")
 	public String edit(@PathVariable Long itemId, @ModelAttribute Product item) {
 		productRepository.update(itemId, item);
-		return "redirect:/basic/items/{itemId}"; // redirect 설정은 이렇게 할 수 있음
+		//return "redirect:/basic/items/{itemId}"; // redirect 설정은 이렇게 할 수 있음
+		return "redirect:/advanced/items/{itemId}";
 	}
 	
 	// test 용 데이터 추가 (store 에 아무것도 없으니까 몇개 생성 해주기)
